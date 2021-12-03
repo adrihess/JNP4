e5d471eb21d3b949d25f585e7c340338cd4cea3e
Kapiszon2343 <46422065+Kapiszon2343@users.noreply.github.com>
2021-12-03 19:06:21 +0100

    kilka funkcji w Treasure
    i bardziej szczegółowe TODO

--- treasure.h
+++ treasure.h
@@ -11,7 +11,29 @@ concept integer = is_integral<T>::value;
 template <typename ValueType, bool IsTrapped>
 struct Treasure
 {
-  // TODO
+private:
+  ValueType value;
+  ValueType tmp;
+
+public:
+  constexpr Treasure(ValueType val)
+      : value(val) {}
+
+  constexpr ValueType evaluate()
+  {
+    return value;
+  }
+
+  constexpr ValueType getLoot()
+  {
+    return (tmp = value) + (value = 0);
+  }
+
+  // TODO nie wiem, czy pole IsTrapped trzeba jakoś specjalie oznaczać
 };
+
+// TODO SafeTreasure<ValueType>
+
+// TODO TrappedTreasure<ValueType>
 
 #endif // __TREASURE_H__
